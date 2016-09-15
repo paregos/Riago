a6a96d3f904024e700cb967d9b36a2f9232904f1
Paregos <ben.mitchell58@gmail.com>
2016-09-15 22:19:01 +1200

    fixed issue where card set would be queired every pack

--- js/index.ts
+++ js/index.ts
@@ -1,3 +1,6 @@
+var classicSet;
+var cardImgs = [0, 0, 0, 0, 0, 0];
+
 $(document).ready(function(){
 
     var output = $.ajax({
@@ -9,21 +12,21 @@ $(document).ready(function(){
         //Change data.source to data.something , where something is whichever part of the object you want returned.
         //To see the whole object you can output it to your browser console using:
         //console.log(data);
-        console.log(data);
+
+        classicSet = data;
+        console.log(classicSet);
         var cards = [0, 1, 2, 3, 4, 5];
-        var cardImgs = [0, 0, 0, 0, 0, 0];
+        cardImgs = [0, 0, 0, 0, 0, 0];
           for (var i in cards) {
               console.log(i);
               var number = 1;
-              while(typeof(data[number].img) == "undefined"){
+              while(typeof(classicSet[number].img) == "undefined"){
                 number = Math.floor(Math.random() * 392) + 0;
               }
-              console.log(number);
               cardImgs[i] = number;
           }
             for (var i in cards) {
-              console.log(document.getElementById("cardImg"+i));
-              $("#cardImg"+i).attr("src", data[cardImgs[i]].img);
+              $("#cardImg"+i).attr("src", classicSet[cardImgs[i]].img);
             }
        //	document.getElementById("output").innerTEXT = data.Classic[0].img;
 
@@ -39,52 +42,42 @@ $(document).ready(function(){
 })
 
 function flip(){
-  $("#card").flip('toggle');
-}
-
-function doIt() {
   document.querySelector('#flip-toggle0').classList.toggle('flip');
   document.querySelector('#flip-toggle1').classList.toggle('flip');
   document.querySelector('#flip-toggle2').classList.toggle('flip');
   document.querySelector('#flip-toggle3').classList.toggle('flip');
   document.querySelector('#flip-toggle4').classList.toggle('flip');
   document.querySelector('#flip-toggle5').classList.toggle('flip');
-  var output = $.ajax({
-  url: 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/sets/Classic', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-  type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-  data: {}, // Additional parameters here
-  dataType: 'json',
-  success: function(data) {
-      //Change data.source to data.something , where something is whichever part of the object you want returned.
-      //To see the whole object you can output it to your browser console using:
-      //console.log(data);
-      console.log(data);
 
-      var cards = [0, 1, 2, 3, 4, 5];
-      var cardImgs = [0, 0, 0, 0, 0, 0];
-        for (var i in cards) {
-            console.log(i);
-            var number = 1;
-            while(typeof(data[number].img) == "undefined"){
-              number = Math.floor(Math.random() * 392) + 0;
-            }
-            console.log(number);
-            cardImgs[i] = number;
+  var cards = [0, 1, 2, 3, 4, 5];
+  cardImgs = [0, 0, 0, 0, 0, 0];
+    for (var i in cards) {
+        var number = 1;
+        while(typeof(classicSet[number].img) == "undefined"){
+          number = Math.floor(Math.random() * 392) + 0;
         }
+        cardImgs[i] = number;
+    }
+
+}
+
+function doIt() {
+
+
+
 
           for (var i in cards) {
             console.log(document.getElementById("cardImg"+i));
-            $("#cardImg"+i).attr("src", data[cardImgs[i]].img);
+            // number = Math.floor(Math.random() * 100) + 0;
+            // if (number < 10){
+            //   console.log("golden" + cardImgs[i]);
+              $("#cardImg"+i).attr("src", classicSet[cardImgs[i]].imgGold);
+            // } else {
+            //   $("#cardImg"+i).attr("src", classicSet[cardImgs[i]].img);
+            // }
+
           }
      //	document.getElementById("output").innerTEXT = data.Classic[0].img;
 
-    },
+    }
       // document.getElementById("cardImg").attr("src", data.Classic[0].img);},
-  error: function(err) {
-     console.log(err);
-  },
-  beforeSend: function(xhr) {
-  xhr.setRequestHeader("X-Mashape-Authorization", "yGGx5ha2WymshpM55DRCKGqxSN7Sp1O82NljsnCI5IOxSGWbDv"); // Enter here your Mashape key
-  }
-});
-}
